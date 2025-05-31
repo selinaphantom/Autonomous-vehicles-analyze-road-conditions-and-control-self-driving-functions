ebd65d065c912412de25c7b84f31c55970ef6857
selinaphantom <156293375+selinaphantom@users.noreply.github.com>
2025-05-31 22:57:12 +0800

    init data

---
+++ 訓練yolo/generate_train.py
@@ -0,0 +1,15 @@
+import os
+
+image_files =  []
+os.chdir(os.path.join("data","obj","obj"))
+for filename in os.listdir(os.getcwd()):
+    if filename.endswith(".jpg"):
+        image_files.append("data/obj/obj/" + filename)
+os.chdir("..")
+os.chdir("..")
+with open("train.txt","w") as outfile:
+    for image in image_files:
+        outfile.write(image)
+        outfile.write("\n")
+    outfile.close()
+os.chdir("..")
